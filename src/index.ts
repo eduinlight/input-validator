e405dcf601caead9215985d2016ddec94e1d5991
Eduin Garcia Cordero <eduinlight@gmail.com>
2021-04-08 03:20:55 -0400

    errors array to object

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { SchemaType, FieldError } from './types'
 import validate from './validate'
 import validateAsync from './validateAsync'
 import validateWithError from './validate_with_error'
+import errorsArrayToObject from './errors_array_to_object'
 
-export { extendRules, getMessage, setLocale, setMessages, locales, validate, validateWithError, defaults, SchemaType, FieldError, validateAsync }
+export { extendRules, getMessage, setLocale, setMessages, locales, validate, validateWithError, defaults, SchemaType, FieldError, validateAsync, errorsArrayToObject }
 
 export default validate
